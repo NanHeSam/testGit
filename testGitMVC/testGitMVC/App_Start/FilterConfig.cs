559d54ea1b1f8cee52470bde8112e2e3a4356fff
NanHeSam <samhenan@gmail.com>
2016-12-05 11:10:14 -0500

    first commit

---
+++ testGitMVC/testGitMVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace testGitMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ testGitMVC/testGitMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="testGitMVC.MvcApplication" Language="C#" %>
